bf9b9700cbbd88e7bdf180d883e68128a7a2a5bd
Aisukiiii <aaronlinkedin37@gmail.com>
2025-05-13 18:31:48 -0700

    el programa es capaz de realizar las funciones sin iteraciones

--- MetodoSecante.java
+++ MetodoSecante.java
@@ -11,6 +11,13 @@ public class MetodoSecante {
         double x1 = scanner.nextDouble();
         System.out.println("Ingrese el número de iteraciones a realizar:");
         int iteraciones = scanner.nextInt();
-        double xi = x1;
+            double fx0 = Math.exp(-x0) - x0;
+            double fx1 = Math.exp(-x1) - x1;
+            double fxi = x1 - (fx1 * (x0 - x1)) / (fx0 - fx1);
+            System.out.printf("x0 = %.10f\n", x0);
+            System.out.printf("x1 = %.10f\n", x1);
+            System.out.printf("f(x0) = %.10f\n", fx0);
+            System.out.printf("f(x1) = %.10f\n", fx1);
+            System.out.printf("xi+1 = %.10f\n", fxi);
     }
 }
